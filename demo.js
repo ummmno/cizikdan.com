3fbe56cd4dcb7cba6b0b853c8e5ac89f99435119
ummmno <daniel.opicka@gmail.com>
2024-01-11 12:08:51 +0100

    removed some stuff

--- demo.js
+++ demo.js
@@ -16,38 +16,3 @@
  * You should have received a copy of the GNU General Public License
  * along with shader-web-background.  If not, see <https://www.gnu.org/licenses/>.
  */
-
-window.addEventListener("DOMContentLoaded", () => {
-    const pre = document.createElement("pre");
-    pre.id = "source";
-    pre.style.background = "rgba(0, 0, 0, .6)";
-    pre.style.overflowX = "auto";
-    pre.classList.add("language-html");
-  
-    const source = document.firstElementChild.outerHTML
-      .replace("<head>", "\n<head>")
-      .replace(
-        '<script src="../dist/shader-web-background.min.js"></script>',
-        '<script src="https://xemantic.github.io/shader-web-background/dist/shader-web-background.min.js"></script>'
-      )
-      .replace('<link rel="stylesheet" href="demo.css">\n', "")
-      .replace('<script src="../src/test/js/show-demo-source.js"></script>\n', "");
-  
-    pre.textContent = "<!DOCTYPE html>\n" + source;
-    document.body.append(pre);
-  
-    const highlightStyle = document.createElement("link");
-    highlightStyle.rel = "stylesheet";
-    highlightStyle.href = "../lib/highlight/styles/ir-black-xemantic.css";
-  
-    const highlightScript = document.createElement("script");
-    highlightScript.src = "../lib/highlight/highlight.pack.js"
-  
-    document.head.append(highlightStyle);
-    document.head.append(highlightScript);
-  
-    highlightScript.onload = () => {
-      hljs.highlightBlock(pre);
-    }
-  
-  }, true);
